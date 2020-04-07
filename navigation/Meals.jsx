37112199265110b981a44f811eb2c7fd7011bfc3
Omenkish <omenkish@gmail.com>
2020-04-07 14:06:47 +0100

    Implemented

--- navigation/Meals.jsx
+++ navigation/Meals.jsx
@@ -1,41 +1,82 @@
 import React, { useState } from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import CategoriesScreen from '../screens/Categories';
 import CategoryMealsScreen from '../screens/CategoryMeals';
 import MealDetailScreen from '../screens/MealDetails';
 import Colors from '../constants/colors';
+import FavouritesScreen from '../screens/Favourites';
  
-const Stack =  createStackNavigator();
+const MealsNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator
+      initialRouteName="Categories"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Platform.OS === 'android' ? Colors.primaryColor
+          : ''
+          },
+          headerTintColor: Platform.OS === 'android' ? 'white' : ''
+      }}
+    >
+      <Stack.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ title: 'Meal Categories' }}
+      />
+      <Stack.Screen
+        name="MealDetail"
+        component={MealDetailScreen}
+        options={({ route }) => ({ title: route.params.title })}
+      />
+      <Stack.Screen
+        name="CategoryMeals"
+        component={CategoryMealsScreen}
+        options={({ route }) => ({ title: route.params.title })} 
+      />
+    </Stack.Navigator>
+  );
+};
 
-const MealsNavigator = () => (
-  <Stack.Navigator
-    initialRouteName="Categories"
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.headerColor
-        : ''
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : ''
-    }}
-  >
-    <Stack.Screen
-      name="Categories"
-      component={CategoriesScreen}
-      options={{ title: 'Meal Categories' }}
-    />
-    <Stack.Screen
-      name="MealDetail"
-      component={MealDetailScreen}
-      options={({ route }) => ({ title: route.params.title })}
-    />
-    <Stack.Screen
-      name="CategoryMeals"
-      component={CategoryMealsScreen}
-      options={({ route }) => ({ title: route.params.title })} 
-    />
-  </Stack.Navigator>
-);
+const MealsFavTabNavigator = () => {
+  const android = Platform.OS === 'android';
+  const Tab = android ? createMaterialBottomTabNavigator() : createBottomTabNavigator();
 
-export default MealsNavigator;
+  return (
+    <Tab.Navigator
+      activeColor='white'
+      barStyle={{ backgroundColor: Colors.primaryColor }}
+      tabBarOptions={{
+        activeTintColor: Colors.headerColor
+      }}
+      shifting={true}
+    >
+      <Tab.Screen
+        name="Meals"
+        component={MealsNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='ios-restaurant' size={25} color={color}/>
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Favourites"
+        component={FavouritesScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='ios-star' size={25} color={color}/>
+          ),
+          tabBarColor: Colors.accentColor
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default MealsFavTabNavigator;
--- screens/CategoryMeals.jsx
+++ screens/CategoryMeals.jsx
@@ -6,10 +6,10 @@ import MealItem from '../components/MealItem';
 
 const CategoryMeal = props => {
   const catId = props.route.params.id;
-  // const { navigate } = props.navigation;
+  const { navigate } = props.navigation;
 
   const renderMealItem = ({ item }) => {
-    return <MealItem  item={item} handlePress={() => props.navigation.navigate('MealDetail', item)}/>;
+    return <MealItem  item={item} handlePress={() => navigate('MealDetail', item)}/>;
   }
 
   const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
@@ -19,10 +19,6 @@ const CategoryMeal = props => {
         data={displayedMeals}
         renderItem={renderMealItem}
         style={{width: '100%'}}
-      />
-      <Button
-        title='Go to meal Details'
-        onPress={() => navigate('MealDetail', item)}
       />
     </View>);
  };
